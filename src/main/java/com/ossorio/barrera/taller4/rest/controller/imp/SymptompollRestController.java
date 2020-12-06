a73cb50721f7ce70dd7a9b1c7bb4368bd72a3117
Jose <joseossorio99@hotmail.com>
2020-12-06 08:10:42 -0500

    implement REST controllers for UsvInstitution and Symptompoll

---
+++ src/main/java/com/ossorio/barrera/taller4/rest/controller/imp/SymptompollRestController.java
@@ -0,0 +1,65 @@
+package com.ossorio.barrera.taller4.rest.controller.imp;
+
+import com.ossorio.barrera.taller4.model.Symptompoll;
+import com.ossorio.barrera.taller4.service.interfaces.SymptompollService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/symptoms")
+public class SymptompollRestController {
+
+    private final SymptompollService symptompollService;
+
+    public SymptompollRestController(SymptompollService symptompollService) {
+        this.symptompollService = symptompollService;
+    }
+
+    @GetMapping
+    public List<Symptompoll> getAll() {
+        return symptompollService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Symptompoll findById(@PathVariable Long id) {
+        return symptompollService.findById(id);
+    }
+
+    @GetMapping("/find-by-date")
+    public List<Symptompoll> findByDate(
+            @RequestParam(value = "startDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
+            @RequestParam(value = "endDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate) {
+        return symptompollService.findByDate(startDate, endDate);
+    }
+
+    @GetMapping("/find-by-date-ordered")
+    public List<Symptompoll> findByDateOrdered(@RequestParam(value = "date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        return symptompollService.findByDateOrdered(date);
+    }
+
+    @GetMapping("/zero-questions")
+    public List<Symptompoll> listZeroWeightQuestions() {
+        return symptompollService.listZeroWeightQuestions();
+    }
+
+    @PostMapping
+    public Symptompoll add(@RequestBody Symptompoll symptompoll) {
+        return symptompollService.save(symptompoll, symptompoll.getInstInstId(),
+                symptompoll.getEpidemevent().getEpieveId());
+    }
+
+    @PutMapping
+    public Symptompoll update(@RequestBody Symptompoll symptompoll) {
+        return symptompollService.update(symptompoll, symptompoll.getInstInstId(),
+                symptompoll.getEpidemevent().getEpieveId());
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody Symptompoll symptompoll) {
+        symptompollService.delete(symptompoll);
+    }
+
+}
---
+++ src/main/java/com/ossorio/barrera/taller4/rest/controller/imp/UsvInstitutionRestController.java
@@ -0,0 +1,49 @@
+package com.ossorio.barrera.taller4.rest.controller.imp;
+
+import com.ossorio.barrera.taller4.model.UsvInstitution;
+import com.ossorio.barrera.taller4.service.interfaces.UsvInstitutionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/institutions")
+public class UsvInstitutionRestController {
+
+    private final UsvInstitutionService institutionService;
+
+    public UsvInstitutionRestController(UsvInstitutionService institutionService) {
+        this.institutionService = institutionService;
+    }
+
+    @GetMapping()
+    public List<UsvInstitution> getAll() {
+        return institutionService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public UsvInstitution findById(@PathVariable Long id) {
+        return institutionService.findById(id);
+    }
+
+    @GetMapping("/{name}")
+    public UsvInstitution findByName(@PathVariable String name){
+        return institutionService.findByName(name);
+    }
+
+    @PostMapping
+    public UsvInstitution save(@RequestBody UsvInstitution institution) {
+        return institutionService.save(institution);
+    }
+
+    @PutMapping
+    public UsvInstitution update(@RequestBody UsvInstitution institution){
+        return institutionService.update(institution);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody UsvInstitution institution){
+        institutionService.delete(institution);
+    }
+
+}
